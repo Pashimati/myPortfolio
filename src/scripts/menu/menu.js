227185ff6227fb8edad5923b7182037876deda35
Pashimati <pashimatii@gmail.com>
2021-11-14 22:15:35 +0300

    fix

--- src/scripts/menu/menu.js
+++ src/scripts/menu/menu.js
@@ -1,9 +1,9 @@
-//добавить класс и искать кнопку по классу
-let menuButton = document.querySelector(".main-header__menu");
-let header = document.querySelector('.main-header');
+
+let menuButton = document.querySelector(".menuButton");
+let listMenu = document.querySelector('.listMenu');
 
 const openModal = function () {
-    header.classList.toggle('modal-open');
+    listMenu.classList.toggle('modal-open');
 }
 
 menuButton.addEventListener('click', openModal);
